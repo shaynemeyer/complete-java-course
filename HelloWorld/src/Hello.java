9515f26fbbc43776b2bd0469ea452973aab64f25
Shayne Meyer <shayne@Shaynes-MacBook-Pro-3.local>
2017-03-01 06:50:46 -0800

    initial commit

---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,12 @@
+/**
+ * Created by shayne on 3/1/17.
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello Shayne!");
+        int myFirstNumber = 5;
+        System.out.println(myFirstNumber);
+    }
+}
+
+
---
+++ readme.md
@@ -0,0 +1,2 @@
+# The Complete Java Developer Course
+These files are from [The Complete Java Developer Course](https://www.udemy.com/java-the-complete-java-developer-course) on Udemy.com.
