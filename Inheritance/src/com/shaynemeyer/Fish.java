afcbb81bdda1e903134fd33255fbe0e742d220cd
Shayne Meyer <shayne.meyer@gmail.com>
2017-03-03 07:28:48 -0800

    added Iheritance project

---
+++ Inheritance/src/com/shaynemeyer/Fish.java
@@ -0,0 +1,35 @@
+package com.shaynemeyer;
+
+/**
+ * Created by shaynemeyer on 3/3/17.
+ */
+public class Fish extends Animal {
+    private int gills;
+    private int eyes;
+    private int fins;
+
+    public Fish(String name, int size, int weight, int gills, int eyes, int fins) {
+        super(name, 1, 1, size, weight);
+        this.gills = gills;
+        this.eyes = eyes;
+        this.fins = fins;
+    }
+
+    private void rest() {
+
+    }
+
+    private void moveMuscles(){
+
+    }
+
+    public void moveBackFin() {
+
+    }
+
+    private void swim(int speed) {
+        moveMuscles();
+        moveBackFin();
+        super.move(speed);
+    }
+}
