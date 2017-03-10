777a6bd1ece6425e26a937ab8833426ed6f312cd
Shayne Meyer <shayne.meyer@gmail.com>
2017-03-10 07:28:47 -0800

    update ArrayListChallenge

---
+++ ArrayListChallenge/src/com/shaynemeyer/Contact.java
@@ -0,0 +1,26 @@
+package com.shaynemeyer;
+
+/**
+ * Created by shayne on 3/10/17.
+ */
+public class Contact {
+    private String name;
+    private String phoneNumber;
+
+    public Contact(String name, String phoneNumber){
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public static Contact createContact(String name, String phoneNumber) {
+        return new Contact(name, phoneNumber);
+    }
+}
